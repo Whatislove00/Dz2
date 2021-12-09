e61d2074d83c03574ab287309405e75a2a034eb9
Whatislove00 <vlad.ik.2001@bk.ru>
2021-12-09 22:28:37 +0300

    DZ4

--- 2.c
+++ 2.c
@@ -2,166 +2,136 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(void)
-{
-	char cur;		   
-	int k = 1, i, j, n, length = 0, one, h = 0, g=0;		    
-	FILE* file;		    
-	
-	printf("Number of vertices : ");
-	scanf_s("%d", &n);					
-	int* flag = (int*)malloc(n * sizeof(int));
-	int** a = (int**)malloc(n * sizeof(int*));		
-	for (int arr = 0; arr < n; arr++)
-	{
-		a[arr] = (int*)malloc(sizeof(int));
-	}
-	cur = getchar();			
-	for (i = 0; i < n; i++)
-	{
-		printf("%d line : ", i+1);
-		cur = getchar();			
-		for (j = 0; cur != '\n'; j++)	
-		{
-			if ((cur == '1') || (cur == '0'))	
-			{
-				a[i] = (int*)realloc(a[i], sizeof(int) * k);	
-				(a[i])[k - 1] = (int)cur - '0';		
-				++k;						
-			}
-			cur = getchar();	
-		}
-		cur = '\0';				
-		length = k - 1;				
-		k = 1;					
-	}
-	printf("\n\n");
+int is_con(int **v_arr, int *is_c_arr, int cur_v, int v_count) {
+    int ans = 0;
+    for (int j = 0; j < v_count; ++j) {
+        if (v_arr[cur_v][j] && !is_c_arr[j]) {
+            is_c_arr[j] = 1;
+            ans++;
+            ans += is_con(v_arr, is_c_arr, j, v_count);
+        }
+    }
+    return ans;
+}
 
-	printf("\n   ");
-	for (j = 0; j <= length - 1; j++)
-	{
-		printf("%3c", j + 0141);
-	}
-	printf("\n");
-	for (j = 1; j <= (length * 4); j++)
-	{
-		printf(".");
-	}
-	printf("\n");
-	for (i = 0; i < n; i++)
-	{
-		printf("%i: ", (i + 1));
-		for (j = 0; j < length; j++)
-		{
-			printf("%3d", a[i][j]);
-		}
-		printf("\n");
-	}
+int compare(const void *a, const void *b) {
 
-	flag[0] = 1;
-	for (i = 0; i < n; i++)
-	{
-		if (flag[i] == 1)
-		{
-			for (j = 0; j < n; j++)
-			{	
-				if (flag[j] != 1)
-				{
-					for (int m = 0; m < length; m++)
-					{	
-						if ((a[i][m] == 1) && (a[j][m] == 1))
-						{
-							flag[j] = 1;
-							break;
-						}
-						else
-						{
-							if ((a[i][m] == 1) || (a[j][m] == 1))
-							{
-								flag[j] = 1;
-								break;
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	for (j = 0; j < n; j++)
-	{
-		g = 0;
-		for (int l = 0; l < length; l++)
-		{
-			if (a[j][l] == 0)
-			{
-				g++;
-				if (g == length)
-				{
-					j = n;
-				}
-			}
-		}
-	}
-	for (i = 0; i < n; i++)
-	{
-		if (flag[i] == 1)
-		{
-			h++;		
-		}
-	}
-	if ((h == n) && (g != length))
-		printf("Connected\n");
-	else printf("Not connected\n");
+    return *(int*)a - *(int*)b;
+}
 
-	file = fopen("file.txt", "w");	
-	fprintf(file, "graph A{\n");				
-	int t = -1;				
-	one = 0;				
-	for (j = 0; j < length; j++)			
-	{
-		for (i = 0; i < n; i++)				
-		{
-			if ((a[i][j] == 1) && (one == 0))       
-			{
-				fprintf(file, "%d", (i + 1));	 
-				fprintf(file, " -- ");
-				one++;				
-				t = i;				 
-			}
-			else
-			{
-				if ((a[i][j] == 1) && (one == 1))       
-				{
-					fprintf(file, "%d;\n", (i + 1));	
-					one++;
-					}
-			}
-		}
-		if (t != -1)
-		{
-			if (one == 1)				  
-			{
-				fprintf(file, "%d;\n", (t + 1));   
-			}
-		}
-		one = 0;		
-		t = -1;
-	}
-	for (i = 0; i < n; i++)
-	{
-		fprintf(file, "%d;\n", (i + 1));
-	}
-	fprintf(file, "}");
-	fclose(file);		
-	system("dot C:\\Users\\vladi\\Documents\\GitHub\\Dz2\\dz2\\file.txt -Tbmp -o file.bmp");
-	system("rundll32  \"%ProgramFiles%\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen C:\\Users\\vladi\\Documents\\GitHub\\Dz2\\dz2\\file.bmp");
+int main(int argc, char **argv) {
+    FILE *file1;
+    FILE *file2;
+    file1 = fopen("graph1.dot", "w");
+    file2 = fopen("graph2.dot", "w");
+    fprintf(file1, "graph g {\n");
+    fprintf(file2, "graph g {\n");
 
-	for (i = 0; i < n; i++) 	
-	{
-		free(a[i]);
-	}
-	free(a);	
-	return 0;
+    int v_count = 0;
+    printf("Enter the number of vertices in the graph: ");
+    scanf_s("%d", &v_count);
+    for (int i = 0; i < v_count; ++i) {
+        fprintf(file1, "%d;\n", i + 1);
+        fprintf(file2, "%d;\n", i + 1);
+    }
+
+    printf("Enter the adjacency matrix of a %d by %d graph:\n", v_count, v_count);
+    int *is_c_arr = (int*)malloc(v_count * sizeof(int));
+    int **v_arr = (int**)malloc(v_count * sizeof(int*));
+    for (int i = 0; i < v_count; ++i) {
+        is_c_arr[i] = 0;
+        v_arr[i] = (int*)malloc(v_count * sizeof(int));
+        for (int j = 0; j < v_count; ++j) {
+            int cur = 0;
+            scanf_s("%d", &v_arr[i][j]);
+            if (j >= i) {
+                int sup = v_arr[i][j];
+                while (sup > 0) {
+                    fprintf(file1, "%d -- %d;\n", i + 1, j + 1);
+                    sup--;
+                }
+            }
+        }
+    }
+
+    fprintf(file1, "}\n");
+
+
+    is_c_arr[0] = 1;
+
+    if (is_con(v_arr, is_c_arr, 0, v_count) == v_count - 1) {
+        printf("The given graph turned out to be connected\n");
+    }
+    else {
+        printf("The given graph turned out to be unconnected\n");
+    }
+    int v1 = 0, v2 = 0;
+
+    printf("Enter the edge you want to delete:");
+
+    scanf_s("%d%d", &v1, &v2);
+    v1--;
+    v2--;
+    
+    if (v_arr[v1][v2] == 0 || v_arr[v2][v1] == 0) {
+        printf("There is no specified edge in the graph\n");
+        return 1;
+    }
+    
+    v_arr[v1][v2]--;
+    v_arr[v2][v1]--;
+
+    for (int i = 0; i < v_count; ++i) {
+        
+        for (int j = i; j < v_count; ++j) {
+
+            int sup = v_arr[i][j];
+            while (sup > 0) {
+                fprintf(file2, "%d -- %d;\n", i + 1, j + 1);
+                sup--;
+            }
+        }
+    }
+
+    fprintf(file2, "}\n");
+
+    int **degree_ed_arr = (int**)malloc(v_count * sizeof(int*));
+
+    for (int i = 0; i < v_count; ++i) {
+        degree_ed_arr[i] = malloc(sizeof(int) * 2);
+        degree_ed_arr[i][1] = i;
+    }
+   
+    for (int i = 0; i < v_count; ++i) {
+
+        degree_ed_arr[i][0] = 0;
+       
+        for (int j = 0; j < v_count; ++j) {
+
+            degree_ed_arr[i][0] += v_arr[i][j];
+        }
+    }
+    
+    qsort(degree_ed_arr, v_count, sizeof(int*), compare);
+    
+    for (int i = 0; i < v_count; ++i) {
+       
+        printf("Vertex number %d has degree %d\n", degree_ed_arr[i][1] + 1, degree_ed_arr[i][0]);
+    }
+    
+
+    for (int i = 0; i < v_count; ++i) {
+
+        free(v_arr[i]);
+    }
+
+    fclose(file1);
+    fclose(file2);
+    system("dot -Tpng graph1.dot -o graph1.png");
+    system("graph1.png");
+    system("dot -Tpng graph2.dot -o graph2.png");
+    system("graph2.png");
+
+    return 0;
 }
